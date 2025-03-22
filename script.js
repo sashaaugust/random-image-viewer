68829776465760c95196f2445ddba517ef4809ae
sashaaugust <153401096+sashaaugust@users.noreply.github.com>
2025-03-21 18:33:55 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const imageCount = 2006;
 
 function showRandomImage() {
   const random = Math.floor(Math.random() * imageCount) + 1;
-  const imageUrl = `${baseUrl}${random}.jpg`;  
+  const imageUrl = `${baseUrl}${random}.jpg`;
   document.getElementById('randomImage').src = imageUrl;
 }
 
